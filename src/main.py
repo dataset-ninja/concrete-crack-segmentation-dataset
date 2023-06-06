abc987dbbcd6d581d25c95078f5248d81020f24d
almaz <almazgimaev@gmail.com>
2023-06-06 10:39:15 +0300

    add github to custom data

--- src/main.py
+++ src/main.py
@@ -70,6 +70,7 @@ if len(custom_data) >= 0:
         # "organization_name": None, 
         # "organization_url": None,
         # "tags": [],
+        "github": "dataset-ninja/concrete-crack-segmentation-dataset",
     }
     api.project.update_custom_data(project_id, custom_data)
 
