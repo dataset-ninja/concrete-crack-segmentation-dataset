1fec85ed8fc113a31dfb76d10d71344db0030f81
Ars Vladimirov <grokhael@gmail.com>
2023-08-30 14:09:24 +0600

    unhide

--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,7 @@ from dataset_tools.templates import (
 ##################################
 PROJECT_NAME = "Concrete Crack Segmentation"
 PROJECT_NAME_FULL = "Concrete Crack Segmentation Dataset"
-HIDE_DATASET = True  # set False when 100% sure about repo quality
+HIDE_DATASET = False  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
